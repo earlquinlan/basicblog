05967595ce04e5f30e6ee651f44e095906458593
Earl Quinlan earl.quinlan@gmail.com <earl.quinlan@gmail.com>
2020-03-25 13:53:04 +0100

    destroy methods and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_180927) do
+ActiveRecord::Schema.define(version: 2020_03_25_120115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "username"
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "defis", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_03_23_180927) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
